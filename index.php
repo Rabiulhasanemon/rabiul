3fe066c27035d4f60cbaf4b5ef1219578016f973
Rabiulhasanemon <56408116+Rabiulhasanemon@users.noreply.github.com>
2021-12-13 08:37:32 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once('function/manage.php');
+
+get_header();
+get_sidebar();
+
+ ?>
+    <div class="row">
+        <div class="col-md-12 main_content">
+            Welcome Mr. Emon
+        </div>
+    </div>
+<?php
+
+get_footer(); ?>
